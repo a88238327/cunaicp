18e14f322bc25b4e685ea23c8a0e81890ec66d00
a88238327 <540372951@qq.com>
2019-09-09 16:05:54 +0800

    cunaicp

---
+++ src/main/java/com/mocun/cunaicp/cunaicp/CunaicpApplication.java
@@ -0,0 +1,13 @@
+package com.mocun.cunaicp.cunaicp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CunaicpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CunaicpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mocun/cunaicp/cunaicp/CunaicpApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mocun.cunaicp.cunaicp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CunaicpApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
